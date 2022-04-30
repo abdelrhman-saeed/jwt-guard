0a427c4399343d134f9e97ef476a6bba0b233f3f
abdelrhman-saeed <abdelrhmanSaeed001@gmail.com>
2022-04-30 03:47:31 +0200

    cleaned the package files a little bit

--- src/Auth/Authenticators/AuthenticatorsEnum.php
+++ src/Auth/Authenticators/AuthenticatorsEnum.php
@@ -8,20 +8,4 @@ use AbdelrhmanSaeed\JwtGuard\Auth\Authenticators\DefaultAuthenticator;
 enum AuthenticatorsEnum: string
 {
     case Default = DefaultAuthenticator::class;
-    case Facebook = FacebookAuthenticator::class;
-    case Google = GoogleAuthenticator::class;
 }
-
-// print_r(Authenticators::Default);
-
-// $authenticator = new (Authenticators::Default->value);
-
-// try {
-    
-//     echo Authenticators::df->throw();
-// }
-// catch(Throwable $e) {
-
-//     print_r($e);
-
-// }
