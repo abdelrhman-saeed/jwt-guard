5ad76cafd6b4c1a63809cc0f30a722db2a26b32f
abdelrhman-saeed <abdelrhmanSaeed001@gmail.com>
2022-04-30 05:52:51 +0200

    implemented the DefaultTokenHandler generateToken() Method

--- src/Auth/TokenHandlers/Default/DefaultTokenHandler.php
+++ src/Auth/TokenHandlers/Default/DefaultTokenHandler.php
@@ -2,14 +2,27 @@
 
 namespace AbdelrhmanSaeed\JwtGuard\Auth\TokenHandlers;
 
+use AbdelrhmanSaeed\JwtGuard\Auth\TokenHandlers\Default\TokenK;
+use AbdelrhmanSaeed\JwtGuard\Exceptions\InvalidTokenConfigException;
+use Exception;
+use Firebase\JWT\JWT;
+
 class DefaultTokenHandler extends TokenHandler
 {
 
+    public function __construct(private array $payload) {
+    }
     /**
      * Generates JWT
      */
     public function generateToken(): string {
-        return '';
+
+        if ( ! in_array(config('token_alg'), config('supported_algs')) ) {
+            throw new InvalidTokenConfigException('The Algorithm That You Defined is Not Supported Yet.');
+        }
+
+        $payload = array_merge( config('payload'), $this->payload);
+        return JWT::encode($payload, TokenK::generate(), config('token_alg'));
     }
     
     /**
