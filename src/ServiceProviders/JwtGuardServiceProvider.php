a42f02e1f12fa710616f3c0075974174dd6eff1a
abdelrhman-saeed <abdelrhmanSaeed001@gmail.com>
2022-05-19 23:30:44 +0200

    changed minor changes

--- src/ServiceProviders/JwtGuardServiceProvider.php
+++ src/ServiceProviders/JwtGuardServiceProvider.php
@@ -13,7 +13,10 @@ class JwtGuardServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        $this->publishes([__DIR__ . '/../Auth/Config/DefaultTokenConfig.php' => config_path('DefaultTokenConfig.php')]);
+        $this->publishes([
+            __DIR__ . '/../Auth/Config/DefaultTokenConfig.php' => config_path('DefaultTokenConfig.php'),
+            __DIR__ . '/../Auth/Config/AuthenticatorsConfig.php' => config_path('AuthenticatorsConfig.php'),
+        ]);
         $this->loadMigrationsFrom(__DIR__ . '/../Migrations');
         
     }
