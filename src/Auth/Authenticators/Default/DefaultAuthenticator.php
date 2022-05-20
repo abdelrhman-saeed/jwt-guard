2c018af31776a285868b2852aab2d6574c50f3bd
abdelrhman-saeed <abdelrhmanSaeed001@gmail.com>
2022-05-20 23:25:01 +0200

    implemented the DefaultAuthenticator Class with its unit testing

--- src/Auth/Authenticators/Default/DefaultAuthenticator.php
+++ src/Auth/Authenticators/Default/DefaultAuthenticator.php
@@ -1,31 +1,83 @@
 <?php
 
-namespace abdelrhmanSaeed\JwtGuard\Auth\Authenticators;
+namespace abdelrhmanSaeed\JwtGuard\Auth\Authenticators\Default;
 
+use abdelrhmanSaeed\JwtGuard\Auth\Authenticators\Authenticator;
+use abdelrhmanSaeed\JwtGuard\Auth\Tokens\Default\DefaultToken;
+use abdelrhmanSaeed\JwtGuard\Auth\Tokens\Token;
+use abdelrhmanSaeed\JwtGuard\Exceptions\JwtAuthenticatorException;
+use abdelrhmanSaeed\JwtGuard\Exceptions\TokenKException;
+
+use Firebase\JWT\Key as JWTKey;
+use Illuminate\Http\Request;
 
 class DefaultAuthenticator extends Authenticator
 {
-        /**
-     * Check if the User is authenticated
-     * 
-     * it will check if there's a token in the HTTP Authorization Header With Bearer Schema.
-     * if the token is not found, then will check for the refresh token that's stored in the
-     * browser cookie
-     * 
-     * @return bool
-     */
-    public function authenticated(): bool
+
+    private Token $token;
+    private \abdelrhmanSaeed\JwtGuard\Auth\Tokens\Default\Keys\Algorithms\Key $key;
+    private array $config;
+
+    public function __construct()
     {
-        return true;
+
+
+        $this->config   = (array) config('DefaultTokenConfig');
+        $supported_algs = $this->config['supported_algs'];
+
+        if ( ! isset( $supported_algs[$this->config['token_alg']] )) {
+            throw new TokenKException('The Algorithm That You Defined To Encode The Token is Not Supported Yet!');
+        }
+
+        $this->key   = app( $supported_algs[$this->config['token_alg']], [ 'config' => $this->config] ); 
+        $this->token = app( DefaultToken::class, [ 'config' => $this->config, 'key' => $this->key ]);
+    }
+    
+    /** 
+     * Check if the JWT is valid
+     * 
+     * @return null|array
+     */
+    public function isTokenValid(Request $request): ?array
+    {
+        return $this->token->debugToken(
+                $request->bearerToken() , (new JwtKey( $this->key->getForDecoding(), $this->config['token_alg'] ))
+            );
     }
 
     /**
-     * Authenticate The User to the System
+     * check if the refresh token is valid
      * 
      * @return bool
      */
-    public function authenticate(): bool {
-        return true;
+    public function isRefreshTokenValid(Request $request): ?array {
+        return $this->token->debugRefreshToken($request->cookie('refresh_token'));
+    }
+
+    /**
+     * generates a JWT
+     * 
+     * @return string
+     */
+    public function generateToken(?array $user = null): string {
+
+        if ($user === null) {
+            throw new JwtAuthenticatorException('The User param Must Not Be Null With The Default Authenticator');
+        }
+
+        return $this->token->setPayload($user)->generateToken();
+    }
+
+    /**
+     * generates a refresh token
+     * 
+     * @param bool $longLives - determines if the token will have a long expiration time or not
+     * 
+     * @return string
+     */
+    public function generateRefreshToken(bool $longLives, ?int $userID = null): string
+    {
+        return $this->token->setPayload(['id' => $userID])->generateRefreshToken($longLives);
     }
 
     /**
@@ -33,7 +85,7 @@ class DefaultAuthenticator extends Authenticator
      * 
      * @return bool
      */
-    public function logout(): bool {
-        return true;
+    public function revokeRefreshToken(Request $request): bool {
+        return $this->token->revokeRefreshToken($request->cookie('refresh_token'));
     }
 }
